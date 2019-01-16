e7e03c6956373b5352eb0acee7152c67fbeefa02
sammylu <a379621573@qq.com>
2019-01-16 14:07:47 +0800

    third upload

--- qiuzhiquan/urls.py
+++ qiuzhiquan/urls.py
@@ -21,22 +21,21 @@ from django.contrib import admin
 from django.views.static import serve  # 文件图片解析模块
 from settings import MEDIA_ROOT  # 模板文件图片解析路径
 
-<<<<<<< HEAD
 from interview.views import InterListview, InterDetailView, RegView, LoginView  # 导入View函数/类
-=======
+
 from interview.views import InterListview, InterDetailView  # 导入View函数/类
->>>>>>> 874d5d4bd8eeda6b8d03da9f03b85159ab7b67d2
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),  # 后台admin的url映射
     url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),  # 管理媒体文件路径映射和处理模块
     url(r'^interlist/$', InterListview.as_view(), name="interlist"),  # 名企面经的url映射,name为别名映射
     url(r'^interdetail/(?P<interview_id>\d+)/$', InterDetailView.as_view(), name="interdetail"),  # 面经详情映射
-<<<<<<< HEAD
+
     url(r'^ueditor/', include('DjangoUeditor.urls')),  # 副文本映射
     url(r'register/$', RegView.as_view(), name="register"),  # 用户注册映射
     url(r'login/$', LoginView.as_view(), name="login"),  # 用户登录映射
-=======
+
     url(r'^ueditor/', include('DjangoUeditor.urls')),  # 增加url映射
->>>>>>> 874d5d4bd8eeda6b8d03da9f03b85159ab7b67d2
+
 ]
